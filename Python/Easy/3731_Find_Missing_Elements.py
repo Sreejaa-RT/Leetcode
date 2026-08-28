873a1fbd72c2fd5d5eb3a9099e844ba63ad2b4e6
Sreeja <sreejaathangaraj@gmail.com>
2026-08-28 15:24:41 +0530

    Add Solution for Leetcode 3731: Find Missing Elements

---
+++ Python/Easy/3731_Find_Missing_Elements.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        m=min(nums)
+        n=max(nums)
+        res=[]
+        for i in range(m+1,n):
+            if i not in nums:
+                res.append(i)
+        return res
