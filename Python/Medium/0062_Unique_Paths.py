3b82b87158727018e8aef0de81602528da8ac1c6
Sreeja <sreejaathangaraj@gmail.com>
2026-09-09 14:41:31 +0530

    Add Solution for Leetcode 62: Unique Paths

---
+++ Python/Medium/0062_Unique_Paths.py
@@ -0,0 +1,8 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp=[1]*n
+        for i in range(1,m):
+            for j in range(1,n):
+                dp[j]=dp[j]+dp[j-1]
+        ans=dp[n-1]
+        return ans
